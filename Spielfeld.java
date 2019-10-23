06ff92bf31c580c2df828ec8302ed3335b1aa418
Laura Schamul <laurakimschamul@gmail.com>
2019-10-23 14:46:02 +0200

    Halbes VierGewinnt

--- Spielfeld.java
+++ Spielfeld.java
@@ -1,11 +1,7 @@
 public class Spielfeld {
-    public static void main(String args[]) {
         double size;
 
         public abstract String feld{
             
         }
-
-        
-    }//end main
 }//end class
