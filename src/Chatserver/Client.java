0d5d02cf6cc368c5c387c1a0042aaf56887ba0d0
Laura Schamul <laurakimschamul@gmail.com>
2019-12-02 12:11:52 +0100

    Echo-Server 2.12.19. - 12pm

--- src/Chatserver/Client.java
+++ src/Chatserver/Client.java
@@ -2,21 +2,43 @@ package Chatserver;
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) throws IOException{ //throws IOException statt Try-Catch-Block
+    public static void main(String[] args){ //throws IOException{ //throws IOException statt Try-Catch-Block
+
+        Scanner scan = new Scanner(System.in);
 
         try {
             System.out.println("Client started.");
             Socket socket = new Socket("localhost", 9999); //IP-Adresse des Servers - localhost, da Client und Server auf derselben Maschine sind; Portnummer
+
+            System.out.println("What do you want to say?");
+
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true); //autoflush - sonst werden Daten nicht gesendet
+            //Daten werden an Server gesendet
+
             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
             //System.in liest 1 Byte at a time; wir wollen keine Bytes -> InputStreamReader - nimmt Bytestream und gibt einen Characterstream zurueck
             //wir wollen nicht 1 Character at time, sondern einen gesamten String -> BufferedReader - liest gesamten String at a time
-            System.out.println("Bitte geben Sie etwas ein!");
-            String str = userInput.readLine(); //liest den eingegeben String; wartet, bis der User etwas eingibt
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true); //autoflush - sonst werden Daten nicht gesendet
-            //Daten werden an Server gesendet
-            // out.println(str);
+
+
+            System.out.print("Input: ");
+            String toServer = scan.nextLine();
+
+
+            writer.write(toServer+ "\n");
+            writer.flush();
+
+            String str; //userInput.readLine(); //liest den eingegeben String; wartet, bis der User etwas eingibt
+
+            while((str = userInput.readLine()) != null){
+                System.out.println("Server: " + str); //Solang Nachrichten empfangen, bis Client nichts mehr sagt
+            }
+
+            userInput.close();
+            writer.close();
+
         }
         catch (Exception e){
             e.printStackTrace();
--- src/Chatserver/Server.java
+++ src/Chatserver/Server.java
@@ -5,15 +5,37 @@ import java.io.*;
 
 
 public class Server{
-    public static void main(String[] args) throws IOException{ //throws IOException (entweder das oder try-catch block)
+    public static void main(String[] args){ //throws IOException (entweder das oder try-catch block)
 
 
         try{
-            System.out.println("Waiting for Clients.");
+            System.out.println("Server started.");
+            System.out.println("Waiting for Clients...");
             ServerSocket serverSocket = new ServerSocket(9999); //Portnummer ab bestimmter Zahl (4000 oder so) frei wählbar
             Socket socket = serverSocket.accept(); //Server wartet, dass sich ein Client verbindet und "akzeptiert" ihn dann
             //wenn Connection bekommt, returned es ein Socket-Objekt
             System.out.println("Connection successfull!");
+
+            //Streams
+
+            OutputStream out = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(out);
+
+            InputStream in = socket.getInputStream();
+            BufferedReader userinput = new BufferedReader(new InputStreamReader(in)); //identisch zu Client, aber dort etwas kuerzer
+
+
+            String str;
+
+            while((str = userinput.readLine()) != null){
+                writer.write(str + "\n");
+                writer.flush();
+                System.out.println("Client: " + str); //Solang Nachrichten empfangen, bis Client nichts mehr sagt
+            }
+
+            writer.close();
+            userinput.close(); //am Ende Reader und Writer schliessen
+
         }
         catch (Exception e){ //um IO Exceptions zu haendeln
             e.printStackTrace();
